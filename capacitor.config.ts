9f766b598bf4d157a7c99809ff83823dbb1b4269
Firefox13590 <pharlyb@gmail.com>
2025-07-28 11:58:23 -0400

    reset work to include react instead of pure html/css/js

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lrima.smarthouseapp',
+  appName: 'Smart House App',
+  webDir: 'dist'
+};
+
+export default config;
